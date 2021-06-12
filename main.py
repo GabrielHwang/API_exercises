97ce5376f2db3463b25a5b2d325d7985d6d21ffc
itsVol <huang.erce@gmail.com>
2021-06-12 11:17:51 +0800

    add URL information

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from urllib.request import URLopener
 from utils import *
 import time
 if __name__ == '__main__':
@@ -13,8 +14,8 @@ if __name__ == '__main__':
     time_local=time.localtime(dateTime)
     time_pub= time.strftime("%Y-%m-%d %H:%M:%S",time_local)
    # print("https://space.bilibili.com/"+str(BvInfo['mid']))
-    print("{}{}\n{}{}\n{}{}\n{}{}\n" \
-        .format("用户名： " ,BvInfo['name'],"av: ",BvInfo['aid'],"主页","https://space.bilibili.com/"+str(BvInfo['mid']),"发布时间： ",time_pub))
+    print("{}{}\n{}{}\n{}{}\n{}{}\n{}{}\n" \
+        .format("用户名： " ,BvInfo['name'],"av: ",BvInfo['aid'],"主页","https://space.bilibili.com/"+str(BvInfo['mid']),"发布时间： ",time_pub,"cv: ",BvInfo['cid']))
     
     IP_Information=GetIP(Bvid)
     print("---获取IP地址中---")
@@ -22,3 +23,7 @@ if __name__ == '__main__':
         .format('addr: ',IP_Information['addr'],\
             'country: ',IP_Information['country'],\
                 'isp: ',IP_Information['isp']))
+    CID=BvInfo['cid']
+    URL_addr=GetURL(Bvid,CID)
+    print("---当前视频流为---")
+    print(URL_addr)
--- utils.py
+++ utils.py
@@ -3,7 +3,13 @@ import json
 import re
 import urllib.request
 import hashlib
-
+import os,sys
+#basic idea:
+#1. input correct website link
+#2. regex right uid from input information
+#3. find a right API which can get a json file
+#4. deal with the json information.
+#5. JSON format tool:https://www.bejson.com/
 def GetUUID():
     s = input("---请粘贴Bilibili主页的网址：---\n（形如 https://space.bilibili.com/******\n")
     nums = re.findall(r'\d+', s)
@@ -22,7 +28,7 @@ def GetBvid():
     I = input("---请粘贴Bilibili视频的网址（web端）---\n 形如 https://www.bilibili.com/video/*****\n")
     BvidNum=re.findall(r"[^/]+(?!.*/)",I) #正则到最后反斜杠后的str
     return BvidNum[0]
-    
+
 def GetVideoInfo(Bvid):
     VideoInfoAPI="http://api.bilibili.com/x/web-interface/view?bvid="+Bvid
     VideoInfoJson=json.loads(urllib.request.urlopen(VideoInfoAPI).read())
@@ -31,7 +37,8 @@ def GetVideoInfo(Bvid):
     pubdate=VideoInfoJson['data']['pubdate']
     name=VideoInfoJson['data']['owner']['name']
     mid=VideoInfoJson['data']['owner']['mid']
-    info={'aid':aid,'bivd':biv,'pubdate':pubdate,'name':name,'mid':mid}
+    cid=VideoInfoJson['data']['cid']
+    info={'aid':aid,'bivd':biv,'pubdate':pubdate,'name':name,'mid':mid,'cid':cid}
     return info
 
 def GetIP(Bvid):
@@ -45,3 +52,9 @@ def GetIP(Bvid):
     return IPInfo
     
     
+def GetURL(Bvid,CID):
+    URL_API="http://api.bilibili.com/x/player/playurl?bvid="+ Bvid+"&cid="+str(CID)+"&fnval=1"
+    URL_JSON=json.loads(urllib.request.urlopen(URL_API).read())
+    URL=URL_JSON['data']['durl'][0]['backup_url'][0]
+    return URL
+
